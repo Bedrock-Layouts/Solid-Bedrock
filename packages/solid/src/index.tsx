65ca797c558bd14453d04021a8420b8a10c7039c
tmair@anonyome.com <tmair@anonyome.com>
2022-02-18 22:48:25 -0600

    docs(docs): init doc site

--- packages/solid/src/index.tsx
+++ packages/solid/src/index.tsx
@@ -94,7 +94,63 @@ export interface StackProps {
 }
 
 export const Stack = styled("div")<StackProps>`
+  @property --gutter {
+    syntax: "<length-percentage>";
+    inherits: false;
+    initial-value: 0;
+  }
+
+  box-sizing: border-box;
+  > * {
+    margin: 0;
+  }
+  --gutter: ${(props) => spacing[props.gutter ?? "none"] ?? spacing.none};
   display: flex;
   flex-direction: column;
-  gap: ${(props) => spacing[props.gutter ?? "none"] ?? spacing.none};
+
+  gap: var(--gutter, 0px);
+  align-content: start;
+
+  & > [data-bedrock-column] {
+    grid-column: span 1 / auto;
+  }
+`;
+
+type MinItemWidth = number | CSSLength;
+export interface GridProps {
+  gutter?: SpacingOptions;
+  minItemWidth?: MinItemWidth;
+}
+
+export const Grid = styled("div")<GridProps>`
+  @property --gutter {
+    syntax: "<length-percentage>";
+    inherits: false;
+    initial-value: 0;
+  }
+
+  @property --minItemWidth {
+    syntax: "<length-percentage>";
+    inherits: false;
+    initial-value: 639px;
+  }
+
+  --gutter: ${(props) => spacing[props.gutter ?? "none"] ?? spacing.none};
+  --minItemWidth: ${(props) =>
+    typeof props.minItemWidth === "string"
+      ? props.minItemWidth
+      : `${props.minItemWidth ?? 0}px`};
+
+  box-sizing: border-box;
+  > * {
+    margin: 0;
+  }
+
+  display: grid;
+  gap: var(--gutter, 0px);
+
+  grid-template-columns: repeat(
+    auto-fit,
+    minmax(min(var(--minItemWidth, 639px), 100%), 1fr)
+  );
 `;
