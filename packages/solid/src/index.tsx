70f0915b7a1e43b232ad56f8b0c8f67c6f5beeac
tmair@anonyome.com <tmair@anonyome.com>
2022-02-24 21:09:26 -0600

    feat(solid): add more components

--- packages/solid/src/index.tsx
+++ packages/solid/src/index.tsx
@@ -1,4 +1,5 @@
-import { DefaultTheme, styled } from "solid-styled-components";
+import type { JSX } from "solid-js/jsx-runtime";
+import { DefaultTheme, css, styled } from "solid-styled-components";
 
 type LowercaseCharacter =
   | "a"
@@ -193,4 +194,168 @@ export const Grid = styled("div")<GridProps>`
     auto-fit,
     minmax(min(var(--minItemWidth, 639px), 100%), 1fr)
   );
+`;
+
+function getSafeMaxWidth(maxWidth?: MaxWidth) {
+  if (maxWidth === undefined) return "100%";
+  if (typeof maxWidth === "number") return `${maxWidth}px`;
+  return maxWidth;
+}
+
+type MaxWidth = number | CSSLength;
+
+export interface CenterProps {
+  maxWidth?: MaxWidth;
+  centerText?: boolean;
+  centerChildren?: boolean;
+}
+
+export const Center = styled("div")<CenterProps>`
+  @property --maxWidth {
+    syntax: "<length-percentage>";
+    inherits: false;
+    initial-value: 100%;
+  }
+
+  --maxWidth: ${(props) => getSafeMaxWidth(props.maxWidth)};
+
+  box-sizing: content-box;
+
+  margin-inline-start: auto;
+  margin-inline-end: auto;
+  margin-inline: auto;
+
+  max-inline-size: var(--maxWidth, 100%);
+
+  ${(props) =>
+    props.centerChildren
+      ? css`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        `
+      : ""}
+
+  ${(props) =>
+    props.centerText
+      ? css`
+          text-align: center;
+        `
+      : ""}
+`;
+
+type Basis = CSSLength | number;
+
+export interface ColumnDropProps {
+  gutter?: SpacingOptions;
+  basis?: Basis;
+  noStretchedColumns?: boolean;
+}
+
+function getSafeBasis(basis?: Basis) {
+  if (basis === undefined) return "100%";
+  if (typeof basis === "number") return `${basis}px`;
+  return basis;
+}
+
+export const ColumnDrop = styled("div")<ColumnDropProps>`
+  @property --basis {
+    syntax: "<length-percentage>";
+    inherits: true;
+    initial-value: 100%;
+  }
+
+  @property --gutter {
+    syntax: "<length-percentage>";
+    inherits: false;
+    initial-value: 0px;
+  }
+
+  --basis: ${(props) => getSafeBasis(props.basis)};
+  --gutter: ${(props) =>
+    props.gutter ? getSpacingValue(props.gutter, props.theme) ?? "0px" : "0px"};
+
+  box-sizing: border-box;
+  > * {
+    margin: 0;
+    flex-basis: var(--basis, 100%);
+    flex-grow: ${(props) => (props.noStretchedColumns ? "0" : "1")};
+    flex-shrink: 1;
+  }
+
+  display: flex;
+  flex-wrap: wrap;
+  gap: var(--gutter, 0px);
+`;
+
+type Ratio = [number, number] | `${number}/${number}`;
+
+export interface FrameProps {
+  ratio?: Ratio;
+  position?: string;
+}
+
+type RatioString = `${number}/${number}`;
+
+function checkIsRatio(ratio: unknown): ratio is Ratio {
+  const isCorrectArray =
+    Array.isArray(ratio) && ratio.length === 2 && ratio.every(Number.isFinite);
+  return (
+    isCorrectArray ||
+    (typeof ratio === "string" && /^\d{1,1000}\/\d{1,1000}$/.test(ratio))
+  );
+}
+
+function getRatioString(ratio: Ratio): RatioString {
+  return Array.isArray(ratio) ? (ratio.join("/") as RatioString) : ratio;
+}
+
+function getSafeRatio(ratio: unknown): RatioString | undefined {
+  const isRatio = checkIsRatio(ratio);
+
+  return isRatio ? getRatioString(ratio) : undefined;
+}
+
+export const Frame = styled.div<FrameProps>`
+  box-sizing: border-box;
+  display: block;
+  inline-size: 100%;
+  position: relative;
+  overflow: hidden;
+
+  ${(props) => {
+    const maybeRatio = getSafeRatio(props.ratio);
+    if (maybeRatio) {
+      return css`
+        aspect-ratio: ${maybeRatio};
+      `;
+    }
+    return "";
+  }};
+
+  > * {
+    position: absolute;
+
+    inset-block-start: 0;
+    inset-block-end: 0;
+    inset-inline-start: 0;
+    inset-inline-end: 0;
+
+    inset-block: 0;
+    inset-inline: 0;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  > :is(img, video) {
+    inline-size: 100%;
+    block-size: 100%;
+    size: 100%;
+
+    object-fit: cover;
+    object-position: ${(props) =>
+      typeof props.position === "string" ? props.position : "50%"};
+  }
 `;
