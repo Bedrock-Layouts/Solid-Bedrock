ee956fbe3add7c2ed0e03c9a8d8c4eab817cf53b
tmair@anonyome.com <tmair@anonyome.com>
2022-02-28 09:22:20 -0600

    feat(inline): add inline
    add inline that extends inlinecluster

--- packages/solid/src/index.tsx
+++ packages/solid/src/index.tsx
@@ -372,6 +372,69 @@ export const InlineCluster = styled.div<InlineClusterProps>`
       : alignMap.start};
 `;
 
+type Stretch = "all" | "start" | "end" | number;
+type SwitchAt = string | number;
+
+export interface InlineProps extends InlineClusterProps {
+  stretch?: Stretch;
+  switchAt?: SwitchAt;
+}
+
+function shouldUseSwitch(switchAt?: SwitchAt) {
+  if (switchAt && switchAt > -1) {
+    return true;
+  }
+
+  if (typeof switchAt === "string" && typeof CSS !== undefined) {
+    return CSS.supports(`height: ${switchAt}`);
+  }
+
+  return false;
+}
+
+export const Inline = styled(InlineCluster)<InlineProps>`
+  @property --switchAt {
+    syntax: "<length-percentage>";
+    inherits: true;
+    initial-value: 0;
+  }
+  flex-wrap: nowrap;
+  ${(props) =>
+    props.stretch === "all"
+      ? `> *  { flex: 1 }`
+      : props.stretch === "start"
+      ? `> :first-child { flex: 1 }`
+      : props.stretch === "end"
+      ? `> :last-child { flex: 1 }`
+      : typeof props.stretch === "number"
+      ? `> :nth-child(${props.stretch + 1}) { flex: 1 }`
+      : ""}
+
+  ${(props) =>
+    shouldUseSwitch(props.switchAt)
+      ? css`
+          --switchAt: ${typeof props.switchAt === "string"
+            ? props.switchAt
+            : `${props.switchAt}px`};
+          flex-wrap: wrap;
+          > * {
+            min-inline-size: fit-content;
+            flex-basis: calc(
+              (var(--switchAt) - (100% - var(--gutter, 0px))) * 999
+            );
+          }
+        `
+      : ""}
+
+  &[style*="--switchAt"] {
+    flex-wrap: wrap;
+    > * {
+      min-inline-size: fit-content;
+      flex-basis: calc((var(--switchAt) - (100% - var(--gutter, 0px))) * 999);
+    }
+  }
+`;
+
 type PaddingObj =
   | { left: SpacingOptions }
   | { right: SpacingOptions }
