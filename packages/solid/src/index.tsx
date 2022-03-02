838bfec2a0726748623888f0e71d82e72dcc2904
tmair@anonyome.com <tmair@anonyome.com>
2022-03-01 21:24:35 -0600

    refactor(spacing): refactor spacing

--- packages/solid/src/index.tsx
+++ packages/solid/src/index.tsx
@@ -1,131 +1,14 @@
-import { DefaultTheme, css, styled } from "solid-styled-components";
+export * from "./spacing-constants";
 
-type LowercaseCharacter =
-  | "a"
-  | "b"
-  | "c"
-  | "d"
-  | "e"
-  | "f"
-  | "g"
-  | "h"
-  | "i"
-  | "j"
-  | "k"
-  | "l"
-  | "m"
-  | "n"
-  | "o"
-  | "p"
-  | "q"
-  | "r"
-  | "s"
-  | "t"
-  | "u"
-  | "v"
-  | "w"
-  | "x"
-  | "y"
-  | "z";
-type AllCharacter = LowercaseCharacter | Uppercase<LowercaseCharacter>;
-type NonEmptyString = `${AllCharacter}${string}`;
+import { css, styled } from "solid-styled-components";
 
-type CSSCustomProperties = `var(--${NonEmptyString})`;
-
-type LengthUnit =
-  | "vmin"
-  | "vmax"
-  | "vh"
-  | "vw"
-  | "%"
-  | "ch"
-  | "ex"
-  | "em"
-  | "rem"
-  | "in"
-  | "cm"
-  | "mm"
-  | "pt"
-  | "pc"
-  | "px";
-
-export type CSSLength = `${number}${LengthUnit}` | CSSCustomProperties;
-
-export function checkIsCSSLength(str: string): str is CSSLength {
-  if (typeof str !== "string") return false;
-
-  return [
-    /^[0-9]{0,10000}\.?[0-9]{1,10000}(vmin|vmax|vh|vw|%|ch|ex|em|rem|in|cm|mm|pt|pc|px)$/,
-    /^var\(--\D{1,100}\)$/,
-  ].some((regex) => regex.test(str));
-}
-export interface Spacing {
-  none: CSSLength;
-  xxs: CSSLength;
-  xs: CSSLength;
-  sm: CSSLength;
-  md: CSSLength;
-  mdLg: CSSLength;
-  lg: CSSLength;
-  lgXl: CSSLength;
-  xl: CSSLength;
-  xlXXl: CSSLength;
-  xxl: CSSLength;
-}
-
-export const spacing: Spacing = {
-  none: "0px",
-  xxs: "0.0625rem",
-  xs: "0.125rem",
-  sm: "0.25rem",
-  md: "0.5rem",
-  mdLg: "0.75rem",
-  lg: "1rem",
-  lgXl: "1.5rem",
-  xl: "2rem",
-  xlXXl: "3rem",
-  xxl: "4rem",
-};
-
-export type BaseTheme = Record<string, CSSLength | string | number>;
-
-type ThemeOrDefaultSpace<T> = T extends {
-  space: BaseTheme;
-}
-  ? T["space"]
-  : keyof Spacing;
-
-type SpacingOptions = ThemeOrDefaultSpace<DefaultTheme>;
-
-function fromEntries<T>(entries: [s: string, value: T][]): Record<string, T> {
-  return entries.reduce((acc, [key, value]) => {
-    return { ...acc, [key]: value };
-  }, {});
-}
-
-type MaybeValue = CSSLength | undefined;
-
-type GetSpacingValue = (
-  spacingKey: SpacingOptions,
-  theme?: Partial<BaseTheme>
-) => MaybeValue;
-
-export const getSpacingValue: GetSpacingValue = (spacingKey, theme) => {
-  const maybeSpaceingOrDefault = theme?.space ?? theme?.spacing ?? spacing;
-
-  const safeSpacings = fromEntries(
-    Object.entries(maybeSpaceingOrDefault).map(([spaceKey, value]) => [
-      spaceKey,
-      typeof value === "number" ? `${value}px` : value,
-    ])
-  );
-
-  const spacingVal = safeSpacings[spacingKey];
-
-  const isCSSLength = checkIsCSSLength(spacingVal);
-
-  return isCSSLength ? spacingVal : undefined;
-};
+import {
+  BaseTheme,
+  CSSLength,
+  SpacingOptions,
+  getSpacingValue,
+  spacing,
+} from "./spacing-constants";
 
 function getSafeMaxWidth(maxWidth?: MaxWidth) {
   if (maxWidth === undefined) return "100%";
@@ -141,7 +24,7 @@ export interface CenterProps {
   centerChildren?: boolean;
 }
 
-export const Center = styled("div")<CenterProps>`
+export const Center = styled.div<CenterProps>`
   @property --maxWidth {
     syntax: "<length-percentage>";
     inherits: false;
