18b53510f0de5451870a281530eca2d90a688b23
tmair@anonyome.com <tmair@anonyome.com>
2022-03-11 02:49:17 -0600

    docs(doc-site): get stack page working

--- doc-site/App.tsx
+++ doc-site/App.tsx
@@ -1,21 +1,41 @@
-import { Route, Routes } from "solid-app-router";
+import { Link, Route, Routes } from "solid-app-router";
 import { Component } from "solid-js";
 
-import { Center, PadBox, Split, Stack } from "../packages/solid/src";
+import { Center, Inline, PadBox, Split, Stack } from "../packages/solid/src";
 import { LogoOnly } from "./components/LogoOnly";
 import { LandingPage } from "./pages/LandingPage";
+import { StackPage } from "./pages/StackPage";
 
 const App: Component = () => {
   return (
-    <Split fraction="auto-start" gutter="lg" switchAt={1000}>
+    <Split fraction="1/4" gutter="lg" switchAt="1300px">
       <PadBox as="aside" padding="xl">
-        <Center maxWidth="10rem">
-          <LogoOnly />
-        </Center>
+        <Stack gutter="xxl">
+          <Center maxWidth="10rem">
+            <Link href="/" style="color:inherit;">
+              <LogoOnly />
+            </Link>
+          </Center>
+          <Stack gutter="lg">
+            Spacer Components
+            <Inline gutter="md" switchAt="10rem">
+              <Link href="/column-drop">ColumnDrop</Link>
+              <Link href="/columns">Columns</Link>
+              <Link href="/grid">Grid</Link>
+              <Link href="/inline-cluster">InlineCluster</Link>
+              <Link href="/inline">Inline</Link>
+              <Link href="/masonry-grid">MasonryGrid</Link>
+              <Link href="/reel">Reel</Link>
+              <Link href="/split">Split</Link>
+              <Link href="/stack">Stack</Link>
+            </Inline>
+          </Stack>
+        </Stack>
       </PadBox>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/*all" element={<Stack>Not Found</Stack>} />
+        <Route path="/stack" element={<StackPage />} />
+        <Route path="/*all" element={<PadBox padding="xl">Not Found</PadBox>} />
       </Routes>
     </Split>
   );
